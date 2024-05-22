2b009e7dfe16d93ee83eb6b4a6b7762dca5e08ee
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-05-22 10:23:25 -0400

    questao 3 finalizada

---
+++ questao3.js
@@ -0,0 +1,9 @@
+/*
+Questão 03: Escreva uma arrow function chamada somaArray que recebe um array 
+de números como parâmetro e retorna a soma de todos os elementos desse array. 
+*/ 
+function somaArray(a, b,){
+    return a + b; // Retorna a soma dos parametros
+}
+   // Chamada da função passando os parametros
+  console.log(somaArray(1, 5)); // Saída: 6
