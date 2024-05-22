f1cdd8b7164bf1d2f7a08b17669a4703e46febf3
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-05-22 10:23:01 -0400

    questao 1 finalizada

---
+++ questao1.js
@@ -0,0 +1,10 @@
+/*
+Questão 01: Escreva uma arrow function chamada dobro que recebe um número 
+como parâmetro e retorna o dobro desse número. 
+ */ 
+const dobro = (numero) => {
+    return numero * 2;
+  };
+  
+  // Exemplo de uso:
+  console.log(dobro(7)); // Saída: 14
