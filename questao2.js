b58297a55123639b3d87d0e575507a7068b4cc4f
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-05-22 10:23:13 -0400

    questao 2 finalizada

---
+++ questao2.js
@@ -0,0 +1,8 @@
+/*
+Questão 02: Escreva uma arrow function chamada saudacao que recebe um nome 
+como parâmetro e retorna uma string de saudação. 
+*/ 
+function saudacao(nome) {
+    console.log("Olá, " + nome + "!"); //Saída: Olá, Allan!
+}
+saudacao("Allan"); //Chamada da função passando o parametro
